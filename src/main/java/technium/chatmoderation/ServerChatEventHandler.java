cd196454097ef52b0ee9f8e8e4f87e43b065e03b
Technium <techniumunlimited@gmail.com>
2021-02-03 05:14:13 -0800

    Optimized

--- src/main/java/technium/chatmoderation/ServerChatEventHandler.java
+++ src/main/java/technium/chatmoderation/ServerChatEventHandler.java
@@ -8,17 +8,20 @@ import net.minecraftforge.fml.common.Mod;
 
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Mod.EventBusSubscriber()
 public class ServerChatEventHandler {
-    public static List<String> CENSORED_WORDS = (List<String>) Config.defaultConfig.get();
-    public static Set<String> CENSORED_WORDS_SET = new HashSet<>(CENSORED_WORDS);
+    private static final Set<Pattern> CENSORED_WORDS = ((Collection<String>) Config.defaultConfig.get()).stream().map(pattern -> "(?i)" + pattern)
+            .map(Pattern::compile)
+            .collect(Collectors.toSet());
 
     @SubscribeEvent
     public static void onChatMessageServer(ServerChatEvent event) {
         String finalMessage = event.getMessage();
-        for (String word : CENSORED_WORDS) {
-            finalMessage = finalMessage.replaceAll(word + "(?i)", "****");
+        for (Pattern word : CENSORED_WORDS) {
+            finalMessage = word.matcher(finalMessage).replaceAll("****");
+                    //finalMessage.replaceAll(word + "(?i)", "****");
         }
         finalMessage = "<" + event.getUsername() + "> " + finalMessage;
         StringTextComponent finalStringTextComponent = new StringTextComponent(finalMessage);
